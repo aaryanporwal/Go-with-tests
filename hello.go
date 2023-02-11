87c169f0203300ebb5eb7ed9ba81d68cbdf7df06
Aaryan Porwal <aaryan.porwal@zomato.com>
2023-02-12 01:13:34 +0530

    Test now follow the TDD pattern!

--- hello.go
+++ hello.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello, World!"
+func Hello(name string) string {
+	return "Hello, " + name + "!"
 }
 
 func main(){
-	fmt.Println(Hello())
+	fmt.Println(Hello("Aaryan"))
 }
 
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,10 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World!"
+	got := Hello("Aaryan")
+	want := "Hello, Aaryan!"
 
-	if got != want {
+	if got != want{
 		t.Errorf("got %q want %q", got, want)
 	}
 }
